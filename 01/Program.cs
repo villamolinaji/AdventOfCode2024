83152290b7934d783180bcb2b5ada65f60d73fda
Jose Villa <jose.villa@version1.com>
2024-12-01 06:17:18 +0100

    Add project files.

---
+++ 01/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var left = new List<int>();
+var right = new List<int>();
+
+foreach (var line in lines)
+{
+	var splitLine = line.Split("   ");
+
+	left.Add(int.Parse(splitLine[0]));
+	right.Add(int.Parse(splitLine[1]));
+}
+
+left.Sort();
+right.Sort();
+
+var distance = 0;
+
+for (int i = 0; i < left.Count; i++)
+{
+	distance += Math.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine(distance);
+
+
+// Part 2
+
+var result = 0;
+
+foreach (var l in left)
+{
+	var count = right.Count(r => r == l);
+	result += l * count;
+}
+
+Console.WriteLine(result);
---
+++ 02/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+foreach (var line in lines)
+{
+
+}

aa7233632f36494dd8c506ec09760cd5158102cd
Jose Villa <jose.villa@version1.com>
2024-12-08 06:53:11 +0100

    Solve day 7 and 8

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,5 +1,65 @@
 ﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
 
+long total = 0;
+long total2 = 0;
+
 foreach (var line in lines)
 {
+	string[] parts = line.Split(":");
+	long target = long.Parse(parts[0].Trim());
+	string[] numbersString = parts[1].Trim().Split(" ");
+	long[] numbers = Array.ConvertAll(numbersString, long.Parse);
+
+	if (CanTarget(numbers, target, false))
+	{
+		total += target;
+	}
+
+	if (CanTarget(numbers, target, true))
+	{
+		total2 += target;
+	}
+}
+
+Console.WriteLine(total);
+Console.WriteLine(total2);
+
+
+bool CanTarget(long[] numbers, long target, bool isPart2)
+{
+	return CheckTarget(numbers, target, 0, numbers[0], isPart2);
+}
+
+bool CheckTarget(long[] numbers, long target, int index, long currentValue, bool isPart2)
+{
+	if (index == numbers.Length - 1)
+	{
+		return currentValue == target;
+	}
+
+	int nextIndex = index + 1;
+	long nextNumber = numbers[nextIndex];
+	long nextValue = currentValue + nextNumber;
+
+	if (CheckTarget(numbers, target, nextIndex, nextValue, isPart2))
+	{
+		return true;
+	}
+
+	nextValue = currentValue * nextNumber;
+	if (CheckTarget(numbers, target, nextIndex, nextValue, isPart2))
+	{
+		return true;
+	}
+
+	if (isPart2)
+	{
+		nextValue = long.Parse(currentValue.ToString() + nextNumber.ToString());
+		if (CheckTarget(numbers, target, nextIndex, nextValue, isPart2))
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
--- 08/Program.cs
+++ 08/Program.cs
@@ -1,5 +1,125 @@
 ﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
 
-foreach (var line in lines)
+int rows = lines.Length;
+int cols = lines[0].Length;
+var antennas = new List<(int x, int y, char frequency)>();
+
+for (int y = 0; y < rows; y++)
 {
+	for (int x = 0; x < cols; x++)
+	{
+		if (char.IsLetterOrDigit(lines[y][x]))
+		{
+			antennas.Add((x, y, lines[y][x]));
+		}
+	}
+}
+
+var antinodes = new HashSet<(int x, int y)>();
+
+for (int i = 0; i < antennas.Count; i++)
+{
+	for (int j = 0; j < antennas.Count; j++)
+	{
+		if (i == j)
+		{
+			continue;
+		}
+
+		var (x1, y1, frequency1) = antennas[i];
+		var (x2, y2, frequency2) = antennas[j];
+
+		if (frequency1 == frequency2)
+		{
+			int dx = x2 - x1;
+			int dy = y2 - y1;
+
+			int newX = x1 - dx;
+			int newY = y1 - dy;
+
+			if (IsInside(newX, newY))
+			{
+				antinodes.Add((newX, newY));
+			}
+
+			newX = x2 + dx;
+			newY = y2 + dy;
+
+			if (IsInside(newX, newY))
+			{
+				antinodes.Add((newX, newY));
+			}
+		}
+	}
+}
+
+Console.WriteLine(antinodes.Count);
+
+
+// Part 2
+antinodes = new HashSet<(int x, int y)>();
+
+for (int i = 0; i < antennas.Count; i++)
+{
+	for (int j = 0; j < antennas.Count; j++)
+	{
+		if (i == j)
+		{
+			continue;
+		}
+
+		var (x1, y1, frequency1) = antennas[i];
+		var (x2, y2, frequency2) = antennas[j];
+
+		if (frequency1 == frequency2)
+		{
+			antinodes.Add((x1, y1));
+			antinodes.Add((x2, y2));
+
+			int index = 1;
+			while (true)
+			{
+				var doNotBreak = false;
+
+				int dx = (x2 - x1) * index;
+				int dy = (y2 - y1) * index;
+
+				int newX = x1 - dx;
+				int newY = y1 - dy;
+
+				if (IsInside(newX, newY))
+				{
+					antinodes.Add((newX, newY));
+					doNotBreak = true;
+				}
+
+				newX = x2 + dx;
+				newY = y2 + dy;
+
+				if (IsInside(newX, newY))
+				{
+					antinodes.Add((newX, newY));
+					doNotBreak = true;
+				}
+
+				if (!doNotBreak)
+				{
+					break;
+				}
+
+				index++;
+			}
+		}
+	}
+}
+
+Console.WriteLine(antinodes.Count);
+
+
+bool IsInside(int newX, int newY)
+{
+	return newX >= 0 &&
+		newX < cols &&
+		newY >= 0 &&
+		newY < rows;
 }

9e2fe1ecaf3b6931ddb8af5e2d9527f2cb4c4048
Jose Villa <jose.villa@version1.com>
2024-12-10 09:49:43 +0100

    Solve day 9 and 10

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,5 +1,176 @@
-﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
+﻿string input = File.ReadAllTextAsync("Input.txt").Result;
 
-foreach (var line in lines)
+var diskMap = new List<(int id, int blocks)>();
+bool isBlockFile = true;
+int currentId = 0;
+int? freeIndex = null;
+
+foreach (char ch in input)
 {
+	int x = int.Parse(ch.ToString());
+
+	for (int i = 0; i < x; i++)
+	{
+		if (isBlockFile)
+		{
+			diskMap.Add((currentId, x));
+		}
+		else
+		{
+			if (freeIndex == null)
+			{
+				freeIndex = diskMap.Count;
+			}
+
+			diskMap.Add((-1, x));
+		}
+	}
+
+	if (isBlockFile)
+	{
+		currentId++;
+	}
+
+	isBlockFile = !isBlockFile;
 }
+
+while (freeIndex < diskMap.Count)
+{
+	while (diskMap[diskMap.Count - 1].id == -1)
+	{
+		diskMap.RemoveAt(diskMap.Count - 1);
+	}
+
+	if (freeIndex >= diskMap.Count)
+	{
+		break;
+	}
+
+	diskMap[(int)freeIndex] = diskMap[diskMap.Count - 1];
+	diskMap.RemoveAt(diskMap.Count - 1);
+
+	while (freeIndex < diskMap.Count &&
+		diskMap[(int)freeIndex].id != -1)
+	{
+		freeIndex++;
+	}
+}
+
+long result = 0;
+for (int i = 0; i < diskMap.Count; i++)
+{
+	if (diskMap[i].id != -1)
+	{
+		result += i * diskMap[i].id;
+	}
+}
+
+Console.WriteLine(result);
+
+
+// Part 2
+diskMap = new List<(int, int)>();
+currentId = 0;
+isBlockFile = true;
+int? firstGap = null;
+
+foreach (char ch in input)
+{
+	int x = int.Parse(ch.ToString());
+	if (isBlockFile)
+	{
+		diskMap.Add((currentId, x));
+	}
+	else
+	{
+		if (firstGap == null)
+		{
+			firstGap = diskMap.Count;
+		}
+		diskMap.Add((-1, x));
+	}
+
+	if (isBlockFile)
+	{
+		currentId++;
+	}
+
+	isBlockFile = !isBlockFile;
+}
+
+int idToMove = currentId - 1;
+while (idToMove >= 0)
+{
+	int topIndex = diskMap.Count - 1;
+	while (diskMap[topIndex].id != idToMove)
+	{
+		topIndex--;
+	}
+
+	for (int i = 0; i < topIndex; i++)
+	{
+		if (diskMap[i].id == -1 &&
+			diskMap[i].blocks >= diskMap[topIndex].blocks)
+		{
+			int spaceLeft = diskMap[i].blocks - diskMap[topIndex].blocks;
+			var newDiskMap = new List<(int, int)>();
+
+			newDiskMap.AddRange(diskMap.GetRange(0, i));
+			newDiskMap.Add(diskMap[topIndex]);
+			newDiskMap.Add((-1, spaceLeft));
+			newDiskMap.AddRange(diskMap.GetRange(i + 1, topIndex - i - 1));
+			newDiskMap.Add((-1, diskMap[topIndex].Item2));
+			newDiskMap.AddRange(diskMap.GetRange(topIndex + 1, diskMap.Count - topIndex - 1));
+
+			diskMap = newDiskMap;
+
+			break;
+		}
+	}
+
+	idToMove--;
+
+	var updatedDiskMap = new List<(int, int)>();
+	int curSpace = 0;
+
+	foreach (var (id, blocks) in diskMap)
+	{
+		if (id != -1)
+		{
+			if (curSpace > 0)
+			{
+				updatedDiskMap.Add((-1, curSpace));
+			}
+
+			curSpace = 0;
+			updatedDiskMap.Add((id, blocks));
+		}
+		else
+		{
+			curSpace += blocks;
+		}
+	}
+
+	diskMap = updatedDiskMap;
+}
+
+long result2 = 0;
+int loc = 0;
+
+foreach (var (id, blocks) in diskMap)
+{
+	if (id != -1)
+	{
+		for (int x = 0; x < blocks; x++)
+		{
+			result2 += loc * id;
+			loc++;
+		}
+	}
+	else
+	{
+		loc += blocks;
+	}
+}
+
+Console.WriteLine(result2);
--- 10/Program.cs
+++ 10/Program.cs
@@ -1,5 +1,122 @@
 ﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
 
-foreach (var line in lines)
+var map = lines
+	.Select(row => row.Select(ch => int.Parse(ch.ToString())).ToArray())
+	.ToArray();
+
+var rows = map.Length;
+var cols = map[0].Length;
+
+int[] dRow = { -1, 1, 0, 0 };
+int[] dCol = { 0, 0, -1, 1 };
+
+int total = 0;
+
+for (int row = 0; row < rows; row++)
 {
+	for (int col = 0; col < cols; col++)
+	{
+		if (map[row][col] == 0)
+		{
+			total += CalculateScore(row, col);
+		}
+	}
+}
+
+Console.WriteLine(total);
+
+
+// Part 2
+var dp = new int[rows, cols];
+
+for (int height = 9; height >= 0; height--)
+{
+	for (int row = 0; row < rows; row++)
+	{
+		for (int col = 0; col < cols; col++)
+		{
+			if (map[row][col] == height)
+			{
+				if (height == 9)
+				{
+					dp[row, col] = 1;
+				}
+				else
+				{
+					for (int i = 0; i < dRow.Length; i++)
+					{
+						int newRow = row + dRow[i];
+						int newCol = col + dCol[i];
+
+						if (newRow >= 0 &&
+							newRow < rows &&
+							newCol >= 0 &&
+							newCol < cols &&
+							map[newRow][newCol] == height + 1)
+						{
+							dp[row, col] += dp[newRow, newCol];
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int total2 = 0;
+for (int row = 0; row < rows; row++)
+{
+	for (int col = 0; col < cols; col++)
+	{
+		if (map[row][col] == 0)
+		{
+			total2 += dp[row, col];
+		}
+	}
+}
+
+Console.WriteLine(total2);
+
+
+int CalculateScore(int startRow, int startCol)
+{
+	var isVisited = new bool[rows, cols];
+	var queue = new Queue<(int row, int col, int height)>();
+
+	queue.Enqueue((startRow, startCol, 0));
+	isVisited[startRow, startCol] = true;
+
+	var visitedTop = new HashSet<(int, int)>();
+
+	while (queue.Count > 0)
+	{
+		var (currentRow, currentCol, currentHeight) = queue.Dequeue();
+
+		if (map[currentRow][currentCol] == 9)
+		{
+			visitedTop.Add((currentRow, currentCol));
+
+			continue;
+		}
+
+		for (int i = 0; i < dRow.Length; i++)
+		{
+			int newRow = currentRow + dRow[i];
+			int newCol = currentCol + dCol[i];
+
+			if (newRow >= 0 &&
+				newRow < rows &&
+				newCol >= 0 &&
+				newCol < cols &&
+				!isVisited[newRow, newCol] &&
+				map[newRow][newCol] == currentHeight + 1)
+			{
+				queue.Enqueue((newRow, newCol, map[newRow][newCol]));
+
+				isVisited[newRow, newCol] = true;
+			}
+		}
+	}
+
+	return visitedTop.Count;
 }

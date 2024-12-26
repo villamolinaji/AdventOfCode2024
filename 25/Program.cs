5788eb3e9caaf5b5a4e6e73fd601f9490035c03c
Jose Villa <jose.villa@version1.com>
2024-12-26 08:17:39 +0100

    Solved day 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,5 +1,96 @@
-﻿string[] lines = File.ReadAllLinesAsync("Input.txt").Result;
+﻿string[] lines = File.ReadAllLinesAsync("input.txt").Result;
 
-foreach (var line in lines)
+var buffer = new List<string>();
+var locks = new List<int[]>();
+var keys = new List<int[]>();
+int result = 0;
+
+foreach (string line in lines)
 {
+	if (string.IsNullOrWhiteSpace(line))
+	{
+		ProcessBuffer(buffer);
+		buffer.Clear();
+
+		continue;
+	}
+
+	buffer.Add(line.Trim());
+}
+
+ProcessBuffer(buffer);
+
+foreach (var key in keys)
+{
+	foreach (var lockHeights in locks)
+	{
+		bool isGood = true;
+
+		for (int i = 0; i < key.Length; i++)
+		{
+			if (key[i] + lockHeights[i] > 5)
+			{
+				isGood = false;
+				break;
+			}
+		}
+
+		if (isGood)
+		{
+			result++;
+		}
+	}
+}
+
+Console.WriteLine(result);
+
+
+void ProcessBuffer(List<string> buffer)
+{
+	if (buffer.Count > 0)
+	{
+		int[] heights = new int[buffer[0].Length];
+
+		if (buffer[0] == "#####")
+		{
+			for (int j = 0; j < buffer[0].Length; j++)
+			{
+				int height = buffer.Count;
+
+				for (int i = 0; i < buffer.Count; i++)
+				{
+					if (buffer[i][j] == '.')
+					{
+						height = i - 1;
+
+						break;
+					}
+				}
+
+				heights[j] = height;
+			}
+
+			locks.Add(heights);
+		}
+		else
+		{
+			for (int j = 0; j < buffer[0].Length; j++)
+			{
+				int height = buffer.Count;
+
+				for (int i = 0; i < buffer.Count; i++)
+				{
+					if (buffer[buffer.Count - 1 - i][j] == '.')
+					{
+						height = i - 1;
+						break;
+					}
+				}
+
+				heights[j] = height;
+			}
+
+			keys.Add(heights);
+		}
+	}
 }
